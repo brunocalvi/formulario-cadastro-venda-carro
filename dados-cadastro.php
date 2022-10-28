fdf24b451d10599d0c0b45887f5171e805c27c33
Bruno Calvi <calvireis@gmail.com>
2022-10-27 21:53:40 -0300

    Ajuste da pagina de dados

--- dados-cadastro.php
+++ dados-cadastro.php
@@ -1,10 +1,32 @@
-<?php 
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Validação de cadastro</title>
+</head>
+<body>
+    <h2>Dados do regitro:</h2>
 
-echo '<pre>';
-echo 'Dados inseridos: <br/>';
-print_r($_POST);
+    <ul>
+        <li><b>Titulo:</b> <?php echo $_POST['title']; ?></li>
+        <li><b>Valor:</b> <?php echo $_POST['price']; ?></li>
+        <li><b>Descrição:</b> <?php echo $_POST['description']; ?></li>
+        <li><b>Unico dono:</b> <?php echo $_POST['dono']; ?></li>
+        <li><b>Marca:</b> <?php echo $_POST['brand']; ?></li>
+        <li><b>Modelo:</b> <?php echo $_POST['model']; ?></li>
+        <li><b>Quilometragem:</b> <?php echo $_POST['mileage']; ?></li>
+        <li><b>Data Compra:</b> <?php echo $_POST['date']; ?></li>
+        <li><b>Modelo Cambio:</b> <?php echo $_POST['cambio']; ?></li>
+        <li><b>Opcional:</b></li>
+        <ul>
+            <?php foreach($_POST['optional'] as $option) { ?>
+            <li> <?php echo $option; ?></li>
+            <?php } ?>
+        </ul>
+        <li><b>Imagem:</b><?php echo $_FILES['file']['name'] ?></li>
+    </ul>
 
-echo 'imagem: <br/>';
-print_r($_FILES);
-echo '</pre>';
-?>
+</body>
+</html>
